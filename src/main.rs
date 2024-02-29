bae36e498bc765ca0b3b96ea98cf6e922a0c15b5
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-02-29 15:50:16 +0530

    feat: parse comma and dash separated ranges

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,70 @@ use pest_derive::Parser;
 pub struct SwhkdParser;
 
 fn binding_parser(pair: Pair<'_, Rule>) {
+    let mut modifiers = vec![];
+    let mut keysyms = vec![];
     for component in pair.into_inner() {
-        println!("{:#?}", component.as_rule());
         match component.as_rule() {
             Rule::modifier => {
-                println!("{}", component.as_str());
+                modifiers.push(vec![component.as_str()]);
             }
 
             Rule::modifier_range => {
-                let modifiers: Vec<_> = component
+                let modifier: Vec<_> = component
                     .into_inner()
                     .map(|component| component.as_str())
                     .collect(); // The grammar only allows discrete values of modifiers.
                                 // We will not support ranges for them.
-                println!("{:#?}", modifiers);
+                modifiers.push(modifier);
+            }
+
+            Rule::modifier_omit_range => {
+                let modifier: Vec<_> = component
+                    .into_inner()
+                    .map(|component| component.as_str())
+                    .collect();
+                modifiers.push(modifier);
+            }
+
+            Rule::range => {
+                for range_component in component.into_inner() {
+                    match range_component.as_rule() {
+                        Rule::keybind => {
+                            keysyms.push(range_component.as_str().to_string());
+                        }
+                        Rule::key_dashed_range => {
+                            let mut bounds = range_component.into_inner().into_iter();
+                            let lower_bound: char = bounds
+                                .next()
+                                .unwrap()
+                                .as_str()
+                                .parse()
+                                .expect("failed to parse lower bound");
+                            let upper_bound: char = bounds
+                                .next()
+                                .unwrap()
+                                .as_str()
+                                .parse()
+                                .expect("failed to parse upper bound");
+
+                            if !lower_bound.is_ascii() || !upper_bound.is_ascii() {
+                                eprintln!("lower and upper bounds are not ascii");
+                                return;
+                            }
+                            assert!(lower_bound < upper_bound);
+
+                            for key in lower_bound..=upper_bound {
+                                keysyms.push(key.to_string());
+                            }
+                        }
+                        _ => {}
+                    }
+                }
             }
             _ => {}
         }
-        println!("-----");
     }
+    println!("modifiers: {:#?}, keysyms: {:#?}", modifiers, keysyms);
 }
 
 fn main() -> Result<()> {
