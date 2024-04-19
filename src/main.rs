a9a3d7a4bf202adbde06bcd8d2977a4c816f08d3
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-04-19 08:21:01 +0530

    feat: make grammar definitions conform to the proposal architecture

--- src/main.rs
+++ src/main.rs
@@ -22,19 +22,19 @@ fn extract_trigger(component: Pair<'_, Rule>) -> Vec<Token> {
             vec![Token::Modifier(component.as_str().to_string())]
         }
 
-        Rule::modifier_range | Rule::modifier_omit_range => component
+        Rule::modifier_shorthand | Rule::modifier_omit_shorthand => component
             .into_inner()
             .map(|component| Token::Modifier(component.as_str().to_string()))
             .collect(),
-        Rule::range => {
+        Rule::shorthand => {
             let mut keys = vec![];
-            for range_component in component.into_inner() {
-                match range_component.as_rule() {
+            for shorthand_component in component.into_inner() {
+                match shorthand_component.as_rule() {
                     Rule::keybind => {
-                        keys.push(Token::Key(range_component.as_str().to_string()));
+                        keys.push(Token::Key(shorthand_component.as_str().to_string()));
                     }
-                    Rule::key_dashed_range => {
-                        let (lower_bound, upper_bound) = extract_bounds(range_component);
+                    Rule::key_range => {
+                        let (lower_bound, upper_bound) = extract_bounds(shorthand_component);
                         keys.extend(
                             (lower_bound..=upper_bound).map(|key| Token::Key(key.to_string())),
                         );
@@ -85,6 +85,25 @@ fn extract_bounds(pair: Pair<'_, Rule>) -> (char, char) {
     (lower_bound, upper_bound)
 }
 
+fn parse_command_shorthand(pair: Pair<'_, Rule>) -> Vec<Token> {
+    let mut command_variants = vec![];
+
+    for thing in pair.into_inner() {
+        match thing.as_rule() {
+            Rule::command_component => {
+                command_variants.push(Token::Command(thing.as_str().to_string()))
+            }
+            Rule::range => {
+                let (lower_bound, upper_bound) = extract_bounds(thing);
+                command_variants
+                    .extend((lower_bound..=upper_bound).map(|key| Token::Command(key.to_string())));
+            }
+            _ => {}
+        }
+    }
+    command_variants
+}
+
 fn binding_parser(pair: Pair<'_, Rule>) {
     let mut tokens = vec![];
     let mut comm = vec![];
@@ -97,23 +116,7 @@ fn binding_parser(pair: Pair<'_, Rule>) {
                             comm.push(vec![Token::Command(subcomponent.as_str().to_string())]);
                         }
                         Rule::command_with_brace => {
-                            let mut command_variants = vec![];
-
-                            for thing in subcomponent.into_inner() {
-                                match thing.as_rule() {
-                                    Rule::command_component => command_variants
-                                        .push(Token::Command(thing.as_str().to_string())),
-                                    Rule::dashed_range => {
-                                        let (lower_bound, upper_bound) = extract_bounds(thing);
-                                        command_variants.extend(
-                                            (lower_bound..=upper_bound)
-                                                .map(|key| Token::Command(key.to_string())),
-                                        );
-                                    }
-                                    _ => {}
-                                }
-                            }
-                            comm.push(command_variants);
+                            comm.push(parse_command_shorthand(subcomponent));
                         }
                         _ => {}
                     }
