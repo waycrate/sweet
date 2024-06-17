7565392269560383a42b50ec643372f64d70bc8d
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-06-17 20:11:53 +0530

    tests: more tests

--- tests/tests.rs
+++ tests/tests.rs
@@ -1,5 +1,5 @@
 use sweet::{
-    token::{Key, KeyAttribute},
+    token::{Key, KeyAttribute, Modifier},
     Binding, Definition, ParseError, SwhkdParser,
 };
 
@@ -63,4 +63,80 @@ t
     assert_eq!(parsed.bindings, known);
 
     Ok(())
+}
+
+#[test]
+fn test_comments() -> Result<(), ParseError> {
+    let contents = "
+r
+    alacritty
+
+w
+    kitty
+
+#t
+    #/bin/firefox
+        ";
+    let parsed = SwhkdParser::from(&contents)?;
+
+    let known = vec![
+        Binding {
+            definition: Definition {
+                modifiers: vec![],
+                key: Key {
+                    key: "r".to_string(),
+                    attribute: KeyAttribute::None,
+                },
+            },
+            command: "alacritty".to_string().to_string(),
+        },
+        Binding {
+            definition: Definition {
+                modifiers: vec![],
+                key: Key {
+                    key: "w".to_string(),
+                    attribute: KeyAttribute::None,
+                },
+            },
+            command: "kitty".to_string().to_string(),
+        },
+    ];
+
+    assert_eq!(parsed.bindings, known);
+
+    Ok(())
+}
+
+#[test]
+fn test_multiple_keypress() -> Result<(), ParseError> {
+    let contents = "
+super + 5
+    alacritty
+        ";
+
+    let parsed = SwhkdParser::from(&contents)?;
+    let known = vec![Binding {
+        definition: Definition {
+            modifiers: vec![Modifier("super".to_string())],
+            key: Key {
+                key: "5".to_string(),
+                attribute: KeyAttribute::None,
+            },
+        },
+        command: "alacritty".to_string().to_string(),
+    }];
+
+    assert_eq!(parsed.bindings, known);
+
+    Ok(())
+}
+
+#[test]
+fn test_keysym_instead_of_modifier() {
+    let contents = "
+shift + k + m
+    notify-send 'Hello world!'
+            ";
+
+    assert!(SwhkdParser::from(&contents).is_err());
 }
