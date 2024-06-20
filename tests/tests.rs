dfef6b734b4402af0a90cad0b4aeb9d47177ed32
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2024-06-20 08:58:35 +0530

    tests: test invalid keybinds and ridiculously spaced configs

--- tests/tests.rs
+++ tests/tests.rs
@@ -31,30 +31,21 @@ t
         Binding {
             definition: Definition {
                 modifiers: vec![],
-                key: Key {
-                    key: "r".to_string(),
-                    attribute: KeyAttribute::None,
-                },
+                key: Key::new("r", KeyAttribute::None),
             },
             command: "alacritty".to_string().to_string(),
         },
         Binding {
             definition: Definition {
                 modifiers: vec![],
-                key: Key {
-                    key: "w".to_string(),
-                    attribute: KeyAttribute::None,
-                },
+                key: Key::new("w", KeyAttribute::None),
             },
             command: "kitty".to_string().to_string(),
         },
         Binding {
             definition: Definition {
                 modifiers: vec![],
-                key: Key {
-                    key: "t".to_string(),
-                    attribute: KeyAttribute::None,
-                },
+                key: Key::new("t", KeyAttribute::None),
             },
             command: "/bin/firefox".to_string().to_string(),
         },
@@ -83,20 +74,14 @@ w
         Binding {
             definition: Definition {
                 modifiers: vec![],
-                key: Key {
-                    key: "r".to_string(),
-                    attribute: KeyAttribute::None,
-                },
+                key: Key::new("r", KeyAttribute::None),
             },
             command: "alacritty".to_string().to_string(),
         },
         Binding {
             definition: Definition {
                 modifiers: vec![],
-                key: Key {
-                    key: "w".to_string(),
-                    attribute: KeyAttribute::None,
-                },
+                key: Key::new("w", KeyAttribute::None),
             },
             command: "kitty".to_string().to_string(),
         },
@@ -118,10 +103,7 @@ super + 5
     let known = vec![Binding {
         definition: Definition {
             modifiers: vec![Modifier("super".to_string())],
-            key: Key {
-                key: "5".to_string(),
-                attribute: KeyAttribute::None,
-            },
+            key: Key::new("5", KeyAttribute::None),
         },
         command: "alacritty".to_string().to_string(),
     }];
@@ -137,6 +119,150 @@ fn test_keysym_instead_of_modifier() {
 shift + k + m
     notify-send 'Hello world!'
             ";
+
+    assert!(SwhkdParser::from(&contents).is_err());
+}
+
+#[test]
+fn test_modifier_instead_of_keysym() {
+    let contents = "
+shift + k + alt
+    notify-send 'Hello world!'
+            ";
+
+    assert!(SwhkdParser::from(&contents).is_err());
+}
+
+#[test]
+fn test_unfinished_plus_sign() {
+    let contents = "
+
+
+shift + alt +
+    notify-send 'Hello world!'
+            ";
+    assert!(SwhkdParser::from(&contents).is_err());
+}
+
+#[test]
+fn test_plus_sign_at_start() {
+    let contents = "
++ shift + k
+    notify-send 'Hello world!'
+            ";
+
+    assert!(SwhkdParser::from(&contents).is_err());
+}
+
+#[test]
+fn test_common_modifiers() -> Result<(), ParseError> {
+    let contents = "
+shift + k
+    notify-send 'Hello world!'
+
+control + 5
+    notify-send 'Hello world!'
+
+alt + 2
+    notify-send 'Hello world!'
+
+altgr + i
+    notify-send 'Hello world!'
+
+super + z
+    notify-send 'Hello world!'
+            ";
+    let parsed = SwhkdParser::from(&contents)?;
+
+    let command = "notify-send 'Hello world!'".to_string();
+    let known = vec![
+        Binding {
+            definition: Definition {
+                modifiers: vec![Modifier("shift".to_string())],
+                key: Key::new("k", KeyAttribute::None),
+            },
+            command: command.clone(),
+        },
+        Binding {
+            definition: Definition {
+                modifiers: vec![Modifier("control".to_string())],
+                key: Key::new("5", KeyAttribute::None),
+            },
+            command: command.clone(),
+        },
+        Binding {
+            definition: Definition {
+                modifiers: vec![Modifier("alt".to_string())],
+                key: Key::new("2", KeyAttribute::None),
+            },
+            command: command.clone(),
+        },
+        Binding {
+            definition: Definition {
+                modifiers: vec![Modifier("altgr".to_string())],
+                key: Key::new("i", KeyAttribute::None),
+            },
+            command: command.clone(),
+        },
+        Binding {
+            definition: Definition {
+                modifiers: vec![Modifier("super".to_string())],
+                key: Key::new("z", KeyAttribute::None),
+            },
+            command: command.clone(),
+        },
+    ];
+
+    assert_eq!(parsed.bindings, known);
+
+    Ok(())
+}
+
+#[test]
+fn test_command_with_many_spaces() -> Result<(), ParseError> {
+    let contents = "
+p
+    xbacklight -inc 10 -fps 30 -time 200
+        ";
+    let parsed = SwhkdParser::from(&contents)?;
+
+    let known = vec![Binding {
+        definition: Definition {
+            modifiers: vec![],
+            key: Key::new("p", KeyAttribute::None),
+        },
+        command: String::from("xbacklight -inc 10 -fps 30 -time 200"),
+    }];
+
+    assert_eq!(parsed.bindings, known);
+
+    Ok(())
+}
+
+#[test]
+fn test_invalid_keybinding() {
+    let contents = "
+p
+    xbacklight -inc 10 -fps 30 -time 200
+
+pesto
+    xterm
+                    ";
+
+    assert!(SwhkdParser::from(&contents).is_err());
+}
+
+#[test]
+// NOTE: This behavior is stricter than the older parser.
+// Don't silently ignore keysyms not followed by command.
+fn test_no_command() {
+    let contents = "
+k
+    xbacklight -inc 10 -fps 30 -time 200
+
+w
+
+                    ";
 
     assert!(SwhkdParser::from(&contents).is_err());
 }
