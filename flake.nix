d33a6fc2e7a8753e63d1006c551e249d06e61881
Himadri Bhattacharjee <107522312+lavafroth@users.noreply.github.com>
2025-03-21 22:19:58 +0530

    deps: prune nix flake by not evaluating darwin derivations
    ### Changes
    - Remove `flake-utils` inputs
    - Roll out `forAllSystems` function which omits darwin (macos)
    derivations since it can't run wayland

--- flake.nix
+++ flake.nix
@@ -1,27 +1,25 @@
 {
-  description = "devshell for github:lavafroth/sweet";
-
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  description = "devshell for github:waycrate/sweet";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
   outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          # leaving out darwin as it cannot run wayland
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell rec {
-          packages = with pkgs; [
-            stdenv.cc.cc.lib
-          ];
-
-          LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath packages}";
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.stdenv.cc.cc ];
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ]}";
         };
-      }
-    );
+      });
+    };
+
 }
